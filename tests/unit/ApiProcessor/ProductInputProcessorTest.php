638c1946e819715c7ffa15ad10c60b4e3be54230
Javier Portátil <javierleal@gmail.com>
2025-05-25 15:50:01 +0200

    Tests unitarios

---
+++ tests/unit/ApiProcessor/ProductInputProcessorTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\ApiProcessor;
+
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
+use App\ApiProcessor\ProductInputProcessor;
+use App\DTO\ProductInput;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use PHPUnit\Framework\TestCase;
+
+class ProductInputProcessorTest extends TestCase
+{
+    private EntityManagerInterface $entityManager;
+    private EntityRepository $repository;
+    private ProductInputProcessor $processor;
+
+    protected function setUp(): void
+    {
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->repository = $this->createMock(EntityRepository::class);
+
+        $this->entityManager
+            ->method('getRepository')
+            ->with(Product::class)
+            ->willReturn($this->repository);
+
+        $this->processor = new ProductInputProcessor($this->entityManager);
+    }
+
+    public function testPostCreatesNewProduct(): void
+    {
+        $input = new ProductInput();
+        $input->name = 'Nuevo Producto';
+        $input->price = 10.0;
+
+        $this->repository
+            ->method('findOneBy')
+            ->with(['name' => 'Nuevo Producto'])
+            ->willReturn(null);
+
+        $this->entityManager->expects($this->once())->method('persist');
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $product = $this->processor->process($input, new Post());
+
+        $this->assertInstanceOf(Product::class, $product);
+        $this->assertSame('Nuevo Producto', $product->getName());
+        $this->assertSame(10.0, $product->getPrice());
+    }
+
+    public function testPostThrowsIfProductExists(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Producto ya existe');
+
+        $input = new ProductInput();
+        $input->name = 'Existente';
+
+        $this->repository
+            ->method('findOneBy')
+            ->with(['name' => 'Existente'])
+            ->willReturn($this->createMock(Product::class));
+
+        $this->processor->process($input, new Post());
+    }
+
+    public function testDelete(): void
+    {
+        $product = $this->createMock(Product::class);
+
+        $this->entityManager->expects($this->once())->method('remove')->with($product);
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $result = $this->processor->process($product, new Delete());
+
+        $this->assertSame($product, $result);
+    }
+}
